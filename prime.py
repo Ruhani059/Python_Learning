6dd8bc244fc538f393e427a5dfd14c8431ba5584
Ruhani059 <ruhani.buet.11@gmail.com>
2017-06-07 20:15:37 +0600

    prime function is modified to xtream high level

--- prime.py
+++ prime.py
@@ -2,12 +2,10 @@ from math import sqrt
 
 def isPrime (n):
     x = int (sqrt(n))
-    isPrimeFlag = True
     for i in range (2,x+1):
         if n % i == 0:
-            isPrimeFlag = False
-            break
-    return isPrimeFlag
+            return False
+    return True
 
 
 print (2**31-1)
