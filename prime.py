d118b091f6c4c347c9033e57c1a6e59002bb5915
Ruhani059 <ruhani.buet.11@gmail.com>
2017-06-07 10:32:46 +0600

    prime.py code is updated and digitsum.py is added.

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
-
 from math import sqrt
+
 def isPrime (n):
     x = int (sqrt(n))
     isPrimeFlag = True
@@ -8,25 +8,9 @@ def isPrime (n):
             isPrimeFlag = False
             break
     return isPrimeFlag
+
+
 print (2**31-1)
 print isPrime(2**31-1)
 
-n = 27
-x = int (sqrt(n))
-list_prime = {}
-for i in range (2,x+1):
-    if isPrime(i) == True:
-        cnt = 0
-        while n % i == 0:
-            cnt = cnt + 1
-            print i, ;
-            n = n / i
-        list_prime[i] = cnt
-print ' '
-n = 403
-sum = 0
-while n != 0:
-    sum += n % 10
-    n = n / 10
-print sum
-print list_prime
+
